3ee9c122c79dd015b22efd31342b05b9042dc286
John Henry <29128358+jhmejia@users.noreply.github.com>
2024-01-24 16:24:05 -0600

    Update build_mongo.sh

--- build_mongo.sh
+++ build_mongo.sh
@@ -2,10 +2,4 @@
 
 cd /home/web/classifAI/resources # area with dockerfile and docker-compose for mongo
 
-# Build the Docker image using the Dockerfile in the current directory
-# Giving it a tag name "mongo_custom" for reference
-docker build -t mongo_custom .
-
-# Now, using docker-compose to run the containers as defined in your docker-compose.yml file
-# This assumes you have a docker-compose.yml in the same directory
 docker-compose up -d
